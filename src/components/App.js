0a8e6838194bc8e8a1d837d0a4b963d4e508d4e7
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-09-12 21:26:35 +0300

    spin animation

--- src/components/App.js
+++ src/components/App.js
@@ -16,11 +16,43 @@ import {
     UIManager
 } from 'react-native';
 
+import style from './style.js';
+
 class App extends Component {
 
+    constructor(props) {
+        super(props);
+        this.spinValue = new Animated.Value(0);
+    }
+
+    spin() {
+        this.spinValue.setValue(0);
+        Animated.timing(this.spinValue, {toValue: 1, duration: 1000, easing: Easing.linear}).start();
+    }
+
     render() {
+
+        const spinRanges = this.spinValue.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['0deg', '340deg']
+        });
+
         return (
-            <Text>Hello World</Text>
+            <View style={style.wrapper}>
+                <Text style={style.heading} onPress={() => this.spin()}>Spin Animation</Text>
+                <Animated.Image
+                    style={[
+                        style.spinner,
+                        {
+                            transform: [
+                                {rotate: spinRanges}
+                            ]
+                        }
+                    ]}
+                    source={require('../assets/img/trollface.png')}
+                />
+            </View>
+
         );
     }
 }
---
+++ src/components/style.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    wrapper: {
+        flex: 1,
+        alignItems: 'center',
+        flexDirection: 'column'
+    },
+    spinner: {
+        width: 200,
+        height: 200,
+        marginTop: 20,
+        alignSelf: 'center',
+    },
+    heading: {
+        fontSize:20,
+        fontWeight:'bold'
+    }
+})
