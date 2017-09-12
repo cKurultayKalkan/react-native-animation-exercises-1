8389f117298f3f70e126bed2b1483c28a76c4bcd
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-09-12 20:54:28 +0300

    hello world

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    ScrollView,
+    View,
+    Image,
+    TouchableHighlight,
+    Switch,
+    Dimensions,
+    Animated,
+    Easing,
+    LayoutAnimation,
+    UIManager
+} from 'react-native';
+
+class App extends Component {
+
+    render() {
+        return (
+            <Text>Hello World</Text>
+        );
+    }
+}
+
+export default App;
