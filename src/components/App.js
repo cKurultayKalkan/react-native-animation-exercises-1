957c8fb1ec3e9f6db1c82594b7e12deba5d9deed
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-09-13 03:03:57 +0300

    more structured

--- src/components/App.js
+++ src/components/App.js
@@ -23,71 +23,93 @@ class App extends Component {
 
     constructor(props) {
         super(props);
-        this.spinValue = new Animated.Value(0);
+        this.animations = {
+            spin: {
+                value: new Animated.Value(0)
+            },
+            flip: {
+                flipDegree: 0,
+                flip: {
+                    value: new Animated.Value(0)
+                },
+                opacity: {
+                    value: new Animated.Value(1)
+                }
+            },
+        };
+
+
+        this.animations.flip.flip.value.addListener(({value}) => {
+            this.animations.flip.flipDegree = value
+        })
     }
 
     componentWillMount() {
-        this.flipAnimatedValue = new Animated.Value(0);
-        this.flipValue = 0;
-        this.flipOpacityAnimatedValue = new Animated.Value(1);
-        this.flipAnimatedValue.addListener(({value}) => {
-            this.flipValue = value
-        });
-        this.frontRotateInterpolate = this.flipAnimatedValue.interpolate({
-            inputRange: [0, 180],
-            outputRange: ['0deg', '180deg']
-        });
-        this.backRotateInterpolate = this.flipAnimatedValue.interpolate({
-            inputRange: [0, 180],
-            outputRange: ['180deg', '360deg']
-        });
-        this.flipOpacityInterpolate = this.flipOpacityAnimatedValue.interpolate({
-            inputRange: [0, 1],
-            outputRange: [1, 0]
-        });
+
+        this.interpolates = {
+            spin: this.animations.spin.value.interpolate({
+                inputRange: [0, 1],
+                outputRange: ['0deg', '340deg']
+            }),
+            flip: {
+                front: this.animations.flip.flip.value.interpolate({
+                    inputRange: [0, 180],
+                    outputRange: ['0deg', '180deg']
+                }),
+                back: this.animations.flip.flip.value.interpolate({
+                    inputRange: [0, 180],
+                    outputRange: ['180deg', '360deg']
+                }),
+                opacity: this.animations.flip.opacity.value.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [1, 0]
+                })
+            }
+        };
     }
 
     spin() {
-        this.spinValue.setValue(0);
-        Animated.timing(this.spinValue, {toValue: 1, duration: 1000, easing: Easing.linear}).start();
+        this.animations.spin.value.setValue(0);
+        Animated.timing(this.animations.spin.value, {toValue: 1, duration: 1000, easing: Easing.linear}).start();
     }
 
     flipCard() {
-        if (this.flipValue >= 90) {
-            Animated.timing(this.flipAnimatedValue, {toValue: 0, duration: 1000}).start();
+        if (this.animations.flip.flipDegree >= 90) {
+            Animated.timing(this.animations.flip.flip.value, {toValue: 0, duration: 1000}).start();
             this.setOpacity(true);
         }
         else {
-            Animated.timing(this.flipAnimatedValue, {toValue: 180, duration: 1000}).start();
+            Animated.timing(this.animations.flip.flip.value, {toValue: 180, duration: 1000}).start();
             this.setOpacity();
         }
     }
 
     setOpacity(status) {
         if (status) {
-            Animated.timing(this.flipOpacityAnimatedValue, {toValue: 1, duration: 1000}).start();
+            Animated.timing(this.animations.flip.opacity.value, {toValue: 1, duration: 1000}).start();
         } else {
-            Animated.timing(this.flipOpacityAnimatedValue, {toValue: 0, duration: 1000}).start();
+            Animated.timing(this.animations.flip.opacity.value, {toValue: 0, duration: 1000}).start();
         }
     }
 
     render() {
 
-        const spinRanges = this.spinValue.interpolate({
-            inputRange: [0, 1],
-            outputRange: ['0deg', '340deg']
-        });
+        const spinAnimatedStyle = {
+            transform: [
+                {rotate: this.interpolates.spin}
+            ]
+        };
 
         const flipFrontAnimatedStyle = {
-            transform: [{rotateX: this.frontRotateInterpolate}]
+            transform: [{rotateX: this.interpolates.flip.front}]
         };
 
         const flipBackFrontAnimatedStyle = {
-            transform: [{rotateX: this.backRotateInterpolate}]
+            transform: [{rotateX: this.interpolates.flip.back}]
         };
 
         const flipOpacityAnimatedStyle = {
-            opacity: this.flipOpacityInterpolate
+            opacity: this.interpolates.flip.opacity
         };
 
         return (
@@ -95,14 +117,7 @@ class App extends Component {
                 <View style={style.example}>
                     <Text style={style.heading} onPress={() => this.spin()}>Spin Animation</Text>
                     <Animated.Image
-                        style={[
-                            style.spinner,
-                            {
-                                transform: [
-                                    {rotate: spinRanges}
-                                ]
-                            }
-                        ]}
+                        style={[style.spinner, spinAnimatedStyle]}
                         source={require('../assets/img/trollface.png')}
                     />
                 </View>
